cb250ec8035f795632b7890d0866f57be041a711
IntelSDM <GitBackup@shibemaster.me>
2023-08-16 14:06:17 +0100

    Pch Set Up

---
+++ PasswordManager/Misc/Pch/pch.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+#include <iostream>
+#include <fstream>
+#include <ppltasks.h>
+#include <windowsx.h>
+#include <psapi.h>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <list>
+#include <string>
+#include <map>
+#include <TlHelp32.h>
+#include <thread>
+
